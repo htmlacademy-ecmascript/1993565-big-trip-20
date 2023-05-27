6f8713d3a8afa0b2428bf25318816c8790cf10b1
piupiupx <akikokuroki1@gmail.com>
2023-05-27 23:31:03 +0300

    сортировка работает

--- src/presenter/event-presenter.js
+++ src/presenter/event-presenter.js
@@ -4,6 +4,9 @@ import TripEventListView from '../view/trip-events-list-view.js';
 import NewEmptyListView from '../view/list-empty-view.js';
 import TripPresenter from './trip-presenter.js';
 import {updateItem} from '../utils.js';
+import {SORT_TYPE} from '../const.js';
+import {sortByPrice, sortByDay, sortByDuration} from '../sort-utils.js';
+
 
 export default class BoardPresenter {
   #container = null;
@@ -15,43 +18,82 @@ export default class BoardPresenter {
   #tripPoints = [];
   #tripsPresenters = new Map();
 
-  constructor({ container, destinationsModel }) {
+  #currentSortType = SORT_TYPE.DAY;
+  #sourcedBoardTrips = [];
+
+ constructor({ container, destinationsModel }) {
     this.#container = container;
     this.#destinationsModel = destinationsModel;
   }
 
-
-  #handleChange = (updatedTask) => {
-    this.#tripPoints = updateItem(this.#tripPoints, updatedTask);
-    this.#tripsPresenters.get(updatedTask.id).init(updatedTask);
-  };
-
-  init() {
-    const tripPoints = [...this.#destinationsModel.destinations];
-
-
-    //render(new SortView(), this.#container);
-    render(this.#tripListComponent, this.#container);
+ init() {
+    this.#tripPoints = [...this.#destinationsModel.destinations];
+    this.#sourcedBoardTrips = [...this.#destinationsModel.destinations];
+    this.#renderSort();
+    console.log(this.#tripPoints);
+    this.#renderTripList();
+    this.#renderTripPoints();
 
 
-    for (const tripPoint of tripPoints) {
-      const tripPointsPresenter = new TripPresenter({tripPointsContainer: this.#tripListComponent.element, onDataChange: this.#handleChange, onModeChange: this.#handleModeChange});
-      tripPointsPresenter.init(tripPoint);
-      this.#tripsPresenters.set(tripPoint.id, tripPointsPresenter);
-
-    }
-    if (tripPoints.length === 0) {
+    if (this.#tripPoints.length === 0) {
       render(new NewEmptyListView(), this.#container);
     }
 
   }
 
+  #handleChange = (updatedTrip) => {
+    this.#tripPoints = updateItem(this.#tripPoints, updatedTrip);
+    this.#sourcedBoardTrips = updateItem(this.#sourcedBoardTrips, updatedTrip);
+    this.#tripsPresenters.get(updatedTrip.id).init(updatedTrip);
+  };
+
+
+  #sortTrips(sortType) {
+    switch (sortType) {
+      case SORT_TYPE.DAY:
+        this.#tripPoints.sort(sortByDay);
+        break;
+      case SORT_TYPE.TIME_LONG:
+        this.#tripPoints.sort(sortByDuration);
+        break;
+      case SORT_TYPE.PRICE_UP:
+        this.#tripPoints.sort(sortByPrice);
+        break;
+      default:
+        this.#tripPoints = [...this.#sourcedBoardTrips];
+    }
+
+    this.#currentSortType = sortType;
+  }
+
+
+  #renderTripList() {
+       render(this.#tripListComponent, this.#container);
+
+  }
 
   #handleSortTypeChange = (sortType) => {
     // - Сортируем задачи
+
+    if (this.#currentSortType === sortType) {
+      return;
+    }
+
+     this.#sortTrips(sortType);
     // - Очищаем список
+      this.#clearTripList();
     // - Рендерим список заново
+      this.#renderTripPoints();
   };
+
+  #renderTripPoints() {
+
+    for (const tripPoint of this.#tripPoints) {
+      const tripPointsPresenter = new TripPresenter({tripPointsContainer: this.#tripListComponent.element, onDataChange: this.#handleChange, onModeChange: this.#handleModeChange});
+      tripPointsPresenter.init(tripPoint);
+      this.#tripsPresenters.set(tripPoint.id, tripPointsPresenter);
+    }
+  }
 
   #renderSort() {
     this.#sortComponent = new SortView({
@@ -65,5 +107,9 @@ export default class BoardPresenter {
     this.#tripsPresenters.forEach((presenter) => presenter.resetView());
   };
 
+   #clearTripList() {
+    this.#tripsPresenters.forEach((presenter) => presenter.destroy());
+    this.#tripsPresenters.clear();
+  }
 
 }
--- src/view/sort-view.js
+++ src/view/sort-view.js
@@ -7,8 +7,8 @@ function createSortTemplate() {
 
   return (` <form class="trip-events__trip-sort  trip-sort" action="#" method="get">
     <div class="trip-sort__item  trip-sort__item--day">
-              <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day"  data-sort-type="${SORT_TYPE.DAY}">
-              <label class="trip-sort__btn" for="sort-day">Day</label>
+              <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day" >
+              <label class="trip-sort__btn" for="sort-day" data-sort-type="${SORT_TYPE.DAY}">Day</label>
             </div>
 
             <div class="trip-sort__item  trip-sort__item--event">
@@ -17,13 +17,13 @@ function createSortTemplate() {
             </div>
 
             <div class="trip-sort__item  trip-sort__item--time">
-              <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time" data-sort-type="${SORT_TYPE.TIME}">
-              <label class="trip-sort__btn" for="sort-time">Time</label>
+              <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time">
+              <label class="trip-sort__btn" for="sort-time"  data-sort-type="${SORT_TYPE.TIME_LONG}">Time</label>
             </div>
 
             <div class="trip-sort__item  trip-sort__item--price">
-              <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price" data-sort-type="${SORT_TYPE.PRIC}">
-              <label class="trip-sort__btn" for="sort-price">Price</label>
+              <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price">
+              <label class="trip-sort__btn" for="sort-price" data-sort-type="${SORT_TYPE.PRICE_UP}">Price</label>
             </div>
 
             <div class="trip-sort__item  trip-sort__item--offer">
@@ -35,7 +35,7 @@ function createSortTemplate() {
 }
 
 
-export default class NewSortView extends AbstractView {
+export default class SortView extends AbstractView {
 
  #handleSortTypeChange = null;
 
